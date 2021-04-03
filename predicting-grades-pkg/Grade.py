7978f82dd67a7354f3c7f190f0a21322ca51012e
Treeve <treevew@gmail.com>
2021-04-04 00:58:15 +0100

    Added docstrings

--- predicting-grades-pkg/Grade.py
+++ predicting-grades-pkg/Grade.py
@@ -1,10 +1,20 @@
-
+"""
+Grade.py
+=========================================
+This module contains the enum Grae which is used by the predicting grades package.
+"""
 import enum
 
 class Grade(enum.Enum):
-    a = 6
-    b = 5
-    c = 4
-    d = 3
-    e = 2
-    f = 1
+    """
+    This is a enumerator for the different possible grades.
+    """
+
+    _init_ = 'value __doc__'
+
+    a = 6, "The top grade"
+    b = 5, "The second highest grade"
+    c = 4, "The third highest grade and the lowest possible pass"
+    d = 3, "The fourth highest grade"
+    e = 2, "The fith highest grade"
+    f = 1, "The sixth highest grade and the lowest possible grade"
