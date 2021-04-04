818aed847dc91857e2b6ed6f0a98202ed8f569fc
Treeve <treevew@gmail.com>
2021-04-04 01:40:46 +0100

    Added docstrings

--- predicting-grades-pkg/Predictor.py
+++ predicting-grades-pkg/Predictor.py
@@ -1,5 +1,7 @@
 """
-A class for making all predictions
+Predictor.py
+=====================================================
+This module conatins th Predictor class which is used by the predicting grades module.
 """
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
@@ -14,18 +16,31 @@ from ModelType import ModelType
 import numpy
 
 class Predictor:
-    def __init__(self, typePredict, dataset):
+    """
+
+    """
+
+    def __init__(self : object, typePredict : ModelType, dataset : DataSet) -> None:
+        """
+
+        """
         self.prediction = None
         self.typePredict = typePredict
         self.dataset = dataset
 
-    def predict(self, valueToPredictFrom):
+    def predict(self : object, valueToPredictFrom : int) -> str:
+        """
+
+        """
         intGradePredict = eval("Grade."+valueToPredictFrom[0]+".value")
         attendance = valueToPredictFrom[1]        
         if self.typePredict == ModelType.KNeighborsClassifier:
             return self.KNeighborsClassifier([[intGradePredict, attendance]])
         
-    def KNeighborsClassifier(self, valueToPredictFrom):
+    def KNeighborsClassifier(self : object, valueToPredictFrom : Grade) -> str:
+        """
+
+        """
         knn = KNeighborsClassifier()
         knn.fit(self.dataset.X_train, self.dataset.Y_train)
         return (knn.predict(valueToPredictFrom))
--- predicting-grades-pkg/TextInterface.py
+++ predicting-grades-pkg/TextInterface.py
@@ -1,11 +1,42 @@
 """
-A class to control all text interface actions.
+TextInterface.py
+================================================
+This module contains the TextInterface class which is used by the predicting grades package.
 """
 
 class TextInterface:
-    def getPredictionValue(self):
+    """
+    This class contains all the text interface methods.
+
+    The object controls all interctions with the user via a text based terminal. This form of interface
+    with teh user could be swapped or interchanged with another GUI or similar so long as the same methods
+    are carried out and can be called by the main program of the package.
+    """
+
+    def getPredictionValue(self : object) -> list:
+        """
+        This method is used to get th users input data for the students mock grade and attendance.
+
+        :param self:    The Text Interfance object
+        :type self:     TextInterface (object)
+
+        :return :   The students mock grade and attendance in a array.
+        :rtype :    list
+        """
         predictGrade = input("What is the Student's Mock grade?")
         attendance = input("What is the attendance?")
         return [predictGrade, attendance]
+
     def showPrediction(self, prediction):
+        """
+        This method displays the calculated prediction for the grade.
+
+        :param self:        The Text Interfance object
+        :type self:         TextInterface (object)
+        :param prediction:  The predicted grade.
+        :type prediction:   string
+
+        :return :   None
+        :rtype :    None
+        """
         print("The prediction is {}".format(prediction))
